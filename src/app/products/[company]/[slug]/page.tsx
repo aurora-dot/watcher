81a792271f9342279338d95bf2fc9478cc8d5e1f
E <e@pastel.codes>
2024-05-22 22:14:45 +0100

    Reformat UI/UX

--- src/app/products/[company]/[slug]/page.tsx
+++ src/app/products/[company]/[slug]/page.tsx
@@ -33,18 +33,21 @@ export default async function Page({
   }
 
   updateMetadata(product);
+  console.log(product.ProductScraper[0].ProductScraperHistory[0].inStock);
 
   return (
     <div className='mx-auto mt-10 flex-grow space-y-3 px-4 text-gray-800 sm:px-6 md:max-w-2xl md:px-4 lg:max-w-4xl lg:px-12'>
       <div className='flex max-w-screen-lg flex-row items-center'>
         <div className='mr-8 w-3/4'>
-          <div className='border-l-2 border-l-green-500 bg-green-50 p-5'>
-            <img
-              src={
-                product.ProductScraper[0].ProductScraperHistory[0].imageBase64
-              }
-            />
-          </div>
+          <a href={product.ProductScraper[0].url}>
+            <div className='border-l-2 border-l-green-500 bg-green-50 p-5'>
+              <img
+                src={
+                  product.ProductScraper[0].ProductScraperHistory[0].imageBase64
+                }
+              />
+            </div>
+          </a>
           <p className='mt-3 text-xs'>
             Last updated:{' '}
             <span className='text-pink-500'>
@@ -53,18 +56,28 @@ export default async function Page({
           </p>
         </div>
         <div className='space-y-4'>
-          <h1
+          <div className={overpassMono.className}>
+            <h1 className={overpassMono.className + ' text-2xl font-bold'}>
+              {product.name}
+            </h1>
+            <h2 className='text-sm'>{product.Company.name}</h2>
+          </div>
+          <div
             className={
               overpassMono.className + ' text-xl font-bold text-green-600'
             }
           >
-            {product.ProductScraper[0].ScraperLambda.currencyType}
-            {product.ProductScraper[0].ProductScraperHistory[0].price}
-          </h1>
-          <h2 className={overpassMono.className + ' text-lg font-bold'}>
-            {product.name}
-          </h2>
-          <h2>{product.Company.name}</h2>
+            <h1>
+              <span className='font-semi-bold text-gray-800'> Price </span>
+              {product.ProductScraper[0].ScraperLambda.currencyType}
+              {product.ProductScraper[0].ProductScraperHistory[0].price}
+            </h1>
+            <h2 className='text-sm'>
+              <span className='font-semi-bold text-gray-800'> In Stock </span>
+              {product.ProductScraper[0].ProductScraperHistory[0].inStock.toString()}
+            </h2>
+          </div>
+
           {/* Hardcoding to ProductScraper[0] for now */}
           <p>
             <a
