b2ac38d359c5eb0bd8e7a85770e778add977382c
E <e@pastel.codes>
2024-05-22 16:29:49 +0100

    Refactor function into new file

---
+++ src/app/products/[company]/[slug]/getProduct.ts
@@ -0,0 +1,36 @@
+import prisma from '@/app/lib/db';
+import type { Product } from '@prisma/client';
+
+export async function fetchProductData(
+  company: string | string[] | undefined,
+  slug: string | string[] | undefined
+): Promise<Product> {
+  if (typeof slug === 'string' && typeof company === 'string') {
+    const product = await prisma.product.findFirst({
+      include: {
+        Company: true,
+        ProductScraper: {
+          include: {
+            ProductScraperHistory: {
+              orderBy: {
+                created: 'desc',
+              },
+              take: 25,
+            },
+            ScraperLambda: {
+              select: {
+                currencyType: true,
+              },
+            },
+          },
+        },
+      },
+      where: { slug: slug, Company: { slug: company } },
+    });
+
+    if (!product) throw new TypeError('fetchProductData: product is null');
+    return product;
+  } else {
+    throw new TypeError('fetchProductData: slug or domain is not a string');
+  }
+}
