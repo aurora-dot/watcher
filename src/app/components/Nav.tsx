ba2796852bf2e14113dded8f5dcbe4130d75972d
E <e@pastel.codes>
2024-05-22 21:58:53 +0100

    Make nav go home

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -5,7 +5,9 @@ const overpassMono = Overpass_Mono({ subsets: ['latin'] });
 export default function Nav() {
   return (
     <nav className='flex flex-grow-0 justify-center py-4'>
-      <p className={overpassMono.className + ' text-3xl font-bold'}>WATCHER</p>
+      <p className={overpassMono.className + ' text-3xl font-bold'}>
+        <a href='/'>WATCHER</a>
+      </p>
     </nav>
   );
 }
